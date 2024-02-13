a9bc9114d4ecf682cb951a43bd784fd08126b915
kostiukevechdima <kostiukevechdima@gmail.com>
2024-02-13 19:35:48 +0200

    commit

--- src/main.js
+++ src/main.js
@@ -9,3 +9,40 @@ document.addEventListener('DOMContentLoaded', function() {
     mobMenu.classList.add('is-open-mob-menu');
   });
 });
+
+const menuLinks = document.querySelectorAll('.mob-nav-item');
+
+// Додаємо обробник подій для кожного посилання
+menuLinks.forEach(link => {
+  link.addEventListener('click', event => {
+    // Забороняємо стандартну поведінку посилання
+    event.preventDefault();
+
+    // Отримуємо значення атрибута href
+    const targetId = link.getAttribute('href');
+
+    // Знаходимо відповідну секцію за її id
+    const targetSection = document.querySelector(targetId);
+
+    // Перевіряємо, чи існує така секція та чи вона відображена
+    if (targetSection && targetSection.offsetParent !== null) {
+      // Прокручуємо до цієї секції
+      window.scrollTo({
+        top: targetSection.offsetTop,
+        behavior: 'smooth', // Зробити прокрутку плавною
+      });
+
+      // Закриваємо мобільне меню
+      closeMobileMenu();
+    }
+  });
+});
+
+// Функція для закриття мобільного меню
+function closeMobileMenu() {
+  // Отримуємо кнопку для закриття меню
+  const closeButton = document.querySelector('.menu-exit-btn');
+
+  // Симулюємо клік на кнопку для закриття меню
+  closeButton.click();
+}
